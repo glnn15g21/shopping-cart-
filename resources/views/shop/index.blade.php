8fbf1f798c5cb1721f7933e937fdd0873dd464e4
glnn15g21 <glnn15g@yahoo.com>
2016-08-29 15:58:51 +0800

    initial commit

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.master')
+
+@section('title')
+	Laravel Shopping Cart
+@endsection
+
+@section('content')
+	<div class="row">
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img class="image-responsive" src="http://www.abebooks.com/images/books/harry-potter/sorcerers-stone.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product name</h3>
+	        <p class="description">sdfs sgsd sgsg qrqrgkghk sdsgd sjfgjfgjfjf qwtrwerwer asdasdasdaasd</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to cart</a></div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img class="image-responsive" src="http://www.abebooks.com/images/books/harry-potter/sorcerers-stone.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product name</h3>
+	        <p class="description">sdfs sgsd sgsg qrqrgkghk sdsgd sjfgjfgjfjf qwtrwerwer asdasdasdaasd</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to cart</a></div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img class="image-responsive" src="http://www.abebooks.com/images/books/harry-potter/sorcerers-stone.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product name</h3>
+	        <p class="description">sdfs sgsd sgsg qrqrgkghk sdsgd sjfgjfgjfjf qwtrwerwer asdasdasdaasd</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to cart</a></div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<div class="row">
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img class="image-responsive" src="http://www.abebooks.com/images/books/harry-potter/sorcerers-stone.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product name</h3>
+	        <p class="description">sdfs sgsd sgsg qrqrgkghk sdsgd sjfgjfgjfjf qwtrwerwer asdasdasdaasd</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to cart</a></div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img class="image-responsive" src="http://www.abebooks.com/images/books/harry-potter/sorcerers-stone.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product name</h3>
+	        <p class="description">sdfs sgsd sgsg qrqrgkghk sdsgd sjfgjfgjfjf qwtrwerwer asdasdasdaasd</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to cart</a></div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img class="image-responsive" src="http://www.abebooks.com/images/books/harry-potter/sorcerers-stone.jpg" alt="...">
+	      <div class="caption">
+	        <h3>Product name</h3>
+	        <p class="description">sdfs sgsd sgsg qrqrgkghk sdsgd sjfgjfgjfjf qwtrwerwer asdasdasdaasd</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to cart</a></div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+@endsection
